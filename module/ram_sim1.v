44a98762f93ed61f2cc1d4f4346c64d74457394c
Louis-Zhu <752858854@qq.com>
2018-12-02 22:06:34 +0800

    Create ram_sim1.v

---
+++ module/ram_sim1.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+
+`ifdef _RAM_SIM1_
+`else
+`define __RAM_SIM_
+`include "define.v"
+
+module RAM_SIM1(
+        input  [`PC_BUS] pc,
+
+        output [`INST_BUS] inst
+    );
+
+reg [`INST_BUS] insts[0:15] ;
+initial begin
+assign insts[0] = 16'b1101_1111_0001_0101;
+//assign insts[1] = ;
+end
+
+assign address = pc[3:0];
+
+assign inst = insts[address];
+
+endmodule
+
+`endif
